063d7e7c2d51cc1937b4d0aa864829cc18996925
chris <chrisgrussell94@gmail.com>
2020-10-04 14:17:39 -0500

    weather app

--- main.py
+++ main.py
@@ -5,41 +5,31 @@ import urllib.request
 app = Flask(__name__)
 
 
-def celcius(temp):
-    return str(round(float(temp) - 273.16, 2))
-
-def farenheiht(temp):
-    return str(round((float(temp) - 273.16) * 1.8 +32))
+def farenheit(temp):
+    return str(round((float(temp) - 273.16) * 1.8 + 32))
 
 
 @app.route('/', methods=['POST', 'GET'])
 def weather():
-    api_key = '13bc68f0e62abb2df7b6c6c75208b714'
     if request.method == 'POST':
         city = request.form['city']
     else:
-
-        city = 'sikeston'
-
+        city = 'Sikeston'
     try:
-        source = urllib.request.urlopen(
-            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key).read()
+        api_request = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=13bc68f0e62abb2df7b6c6c75208b714').read()
     except:
-        return abort(404)
+        abort(404)
 
-
-    alldata = json.loads(source)
-
-    data = {
-        "country_code": str(alldata['sys']['country']),
-        "coordinate": str(alldata['coord']['lon']) + ' ' + str(alldata['coord']['lat']),
-        "temp": str(alldata['main']['temp']) + 'k',
-        "temp_far": farenheiht(alldata['main']['temp']) + 'F',
+    alldata = json.loads(api_request)
+    info = {
+        "temp_far": farenheit(alldata['main']['temp']) + 'F',
         "pressure": str(alldata['main']['pressure']),
         "humidity": str(alldata['main']['humidity']),
         "cityname": str(city),
+        "description": str(alldata['weather'][0]['description'])
     }
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=info)
 
 
 if __name__ == '__main__':
